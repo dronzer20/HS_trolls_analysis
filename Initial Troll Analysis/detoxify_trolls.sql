115c458d4190d484eeb013b50a93dd61a605c52a
dronzer20 <66757308+dronzer20@users.noreply.github.com>
2021-09-18 19:55:05 -0700

    Add files via upload

---
+++ Initial Troll Analysis/detoxify_trolls.sql
@@ -0,0 +1,24 @@
+WITH
+  hs_trolls AS (
+  SELECT
+    DISTINCT authorChannelId,
+    author,
+    COUNT(DISTINCT channel_id) OVER (PARTITION BY authorChannelId, author ) AS number_channels_trolled,
+    COUNT(comment_id) OVER (PARTITION BY authorChannelId, author) AS total_number_toxic_comments
+  FROM
+    `hatespeech-2019.HS_trolls_analysis.Comments_above_detoxify_threshold`
+  GROUP BY
+    authorChannelId,
+    author,
+    channel_id,
+    comment_id
+  ORDER BY
+    number_channels_trolled DESC,
+    total_number_toxic_comments DESC,
+    author ASC )
+SELECT
+  *
+FROM
+  hs_trolls
+WHERE
+  number_channels_trolled >1;
