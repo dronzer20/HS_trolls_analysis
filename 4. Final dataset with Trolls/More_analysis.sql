c7b90b27d2402c8b03e2bdc6991805023d77594c
dronzer20 <66757308+dronzer20@users.noreply.github.com>
2021-09-19 22:39:43 -0700

    Update More_analysis.sql

--- 4. Final dataset with Trolls/More_analysis.sql
+++ 4. Final dataset with Trolls/More_analysis.sql
@@ -1,9 +1,9 @@
-select
-round(sum(is_victim_a_girl)/(count(distinct comment_id))*100,2) as Female_victims_percent,
-round(sum(is_channel_teen)/(count(distinct comment_id))*100,2) as Teenage_victims_percent,
-round(sum(is_victim_from_minority_race)/(count(distinct comment_id))*100,2) as Minority_race_victims_percent,
-round(sum(is_toxic_comment_at_night)/(count(distinct comment_id))*100,2) as Toxicity_at_night_percent,
-count(distinct victim_channel_id) as Total_victims,
-count(distinct troll_channel_id) as Total_trolls
-from
-`hatespeech-2019.HS_trolls_analysis.Troll_comments`
+SELECT
+  ROUND(SUM(is_victim_a_girl)/(COUNT(DISTINCT comment_id))*100,2) AS Female_victims_percent,
+  ROUND(SUM(is_channel_teen)/(COUNT(DISTINCT comment_id))*100,2) AS Teenage_victims_percent,
+  ROUND(SUM(is_victim_from_minority_race)/(COUNT(DISTINCT comment_id))*100,2) AS Minority_race_victims_percent,
+  ROUND(SUM(is_toxic_comment_at_night)/(COUNT(DISTINCT comment_id))*100,2) AS Toxicity_at_night_percent,
+  COUNT(DISTINCT victim_channel_id) AS Total_victims,
+  COUNT(DISTINCT troll_channel_id) AS Total_trolls
+FROM
+  `hatespeech-2019.HS_trolls_analysis.Troll_comments`
