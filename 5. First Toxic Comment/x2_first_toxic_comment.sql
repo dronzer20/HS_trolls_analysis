b234ca84f1295230d87918dcd761f4bc0f0f9fc6
dronzer20 <66757308+dronzer20@users.noreply.github.com>
2021-09-19 22:38:12 -0700

    Update x2_first_toxic_comment.sql

--- 5. First Toxic Comment/x2_first_toxic_comment.sql
+++ 5. First Toxic Comment/x2_first_toxic_comment.sql
@@ -1,16 +1,17 @@
-select
-A.video_id,
-A.first_toxic_comment,
-A.comment_date,
-B.video_timestamp as video_posted,
-date_diff(date(A.comment_date), date(B.video_timestamp), DAY) as Days_before_toxic_comment
-from
-`hatespeech-2019.HS_trolls_analysis.temp_join` A
-left join
-`hatespeech-2019.Final_Dataset.Channel_Videos_Comments_Victims_Merged` B
-using(video_id)
-group by
-A.video_id,
-A.first_toxic_comment,
-A.comment_date,
-video_posted;
+SELECT
+  A.video_id,
+  A.first_toxic_comment,
+  A.comment_date,
+  B.video_timestamp AS video_posted,
+  DATE_DIFF(DATE(A.comment_date), DATE(B.video_timestamp), DAY) AS Days_before_toxic_comment
+FROM
+  `hatespeech-2019.HS_trolls_analysis.temp_join` A
+LEFT JOIN
+  `hatespeech-2019.Final_Dataset.Channel_Videos_Comments_Victims_Merged` B
+USING
+  (video_id)
+GROUP BY
+  A.video_id,
+  A.first_toxic_comment,
+  A.comment_date,
+  video_posted;
